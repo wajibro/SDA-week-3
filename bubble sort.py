478423d32866c54dafb3a6818d5a31df37a36fee
wajibro <wajibro@yahoo.com>
2026-02-19 03:36:37 +0000

    fix code

--- bubble sort.py
+++ bubble sort.py
@@ -3,7 +3,7 @@ import time as t
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        print(f'================ pass ke {i} ================')
+        print(f'================ pass ke {i+1} ================')
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
