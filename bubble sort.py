4f4e366939b36470ca2e013594f8a5f0b884c8d4
wajibro <wajibro@yahoo.com>
2026-02-19 03:39:51 +0000

    fix code

--- bubble sort.py
+++ bubble sort.py
@@ -1,15 +1,16 @@
 import numpy as np
 import time as t
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
+        print()
         print(f'================ pass ke {i+1} ================')
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 print(arr)
                 t.sleep(.5)
-        print()
     return arr
 
 data = np.random.choice(np.arange(0, 10), size=10, replace=False)
